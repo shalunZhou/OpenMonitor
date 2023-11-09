419520bdf30ec81aef94c6fce89546da98c435db
shalunZhou <lunsha498@gmail.com>
2023-11-09 18:41:46 +0800

    Apc Inject Commit in R3

---
+++ DllInject/R3/ApcInject/ApcInject.h
@@ -0,0 +1,18 @@
+﻿/*
+* Author:   shalunZhou
+* Desc:     Apc Inject   
+* Date:     2023/11/09
+* =================================================
+*/
+#pragma once
+#include <Windows.h>
+#include <TlHelp32.h>
+#include <vector>
+#include <string>
+
+
+std::vector<DWORD> GetPidByProcessName(wchar_t* pszProcessName);
+
+std::vector<DWORD> GetTidByPid(const DWORD& pid);
+
+BOOL ApcInject(const DWORD& pid, const std::wstring & dll_path);
